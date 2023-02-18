84a4e47e3ec0e31fd936958ae952b378c4e7c0bf
McUsr <tommy.bollman@gmail.com>
2023-02-18 23:58:51 +0100

    Factored in new functions, and new structure, fixed EXCLUDE_OPTIONS bug.

--- .local/bin/fb/OneShot/OneShot.backup.sh
+++ .local/bin/fb/OneShot/OneShot.backup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # The version at a8de2b1 contains the scaffolding.
 
-PNAME=${0##*/}
-MODE="CONSOLE"
 
 err_report() {
   echo >&2 "$PNAME : Error on line $1"
@@ -11,44 +9,81 @@ err_report() {
 }
 
 trap 'err_report $LINENO' ERR
-VERSION='v0.0.3c'
-if [[ ! -v FB ]] ; then
-     echo -e "$PNAME The variable \$FB isn't set, is the system initialized?\n
-You need configure it.\nTerminating..." | journalThis 2 OneShot
+
+VERBOSE=false
+DEBUG=1
+DRYRUN=false
+
+# dieIfCantSourceShellLibrary()
+# sources the ShellLibraries
+# so we can perform the rest of the tests.
+# TODO: Think of modus.
+dieIfCantSourceShellLibrary() {
+  if [[ $# -ne 1 ]] ; then
+    echo -e >&2 "$PNAME/${FUNCNAME[0]} : Need an\
+    argument, an existing fb shell library file!\nTerminates..." >&2
+    exit 5
+  fi
+  if [[ -r "${1}" ]] ; then
+    source "${1}"
+  #  source "$fpth"/service_functions.sh
+  else
+    echo -e >&2 "$PNAME/${FUNCNAME[0]} : Can't find/source: ${1}\
+      \nTerminates... " >&2
     exit 255
-fi
+  fi
+}
 
-if [[ ! -d "$FB" ]] ; then
-     echo -e "$PNAME" "The folder  $FB  can't be found!\nThe Google Drive \
-folder is probably not shared with with Linux. Isn't set, is the \
-system initialized? Maybe you need to configure it.\nTerminating..."\
-       | journalThis 2 OneShot
-    exit 255
-fi
+pathToSourcedFiles() {
+  # shellcheck disable=SC2001,SC2086  # Escaped by sed
+  pthname="$( echo $0  | sed 's/ /\\ /g' )"
+  # We do escape any spaces, in the file name, 
+  #  knew it could never happen, just in case.
+  # shellcheck disable=SC2086  # Escaped by sed
+  fpth="$(realpath $pthname)"; fpth="${fpth%/*}"
+  echo "$fpth"
+}
 
 
-# shellcheck source=/home/mcusr/.local/bin/fb/shared_functions.sh
-if [[ -r "$XDG_BIN_HOME"/fb/shared_functions.sh ]] ; then
-  source "$XDG_BIN_HOME"/fb/shared_functions.sh
+# Program vars, read only, 
+
+VERSION='v0.0.4'
+PNAME=${0##*/}
+MODE="CONSOLE"
+
+fbBinDir="$(pathToSourcedFiles)"
+
+if [[ $THROUGH_SHELLCHECK -ne 0  ]] ; then
+  dieIfCantSourceShellLibrary "$fbBinDir"/../service_functions.sh
 else
-  echo -e  "$PNAME : Can't source: $XDG_BIN_HOME/fb/shared_functions.sh\
-\nTerminates... "
-  exit 255
+# bootstrapping libraries before figuring system paths.
+# shellcheck source=service_functions.sh
+  source "$fbBinDir"/../service_functions.sh
 fi
 
+# asserting system/configuration context.
+dieIfMandatoryVariableNotSet FB "$RUNTIME_MODE" "$CURSCHEME"
+dieIfMandatoryVariableNotSet XDG_BIN_HOME "$RUNTIME_MODE" "$CURSCHEME"
+
+
+dieIfNotDirectoryExist "$XDG_BIN_HOME"
+
+if [[ $THROUGH_SHELLCHECK -ne 0  ]] ; then
+  dieIfCantSourceShellLibrary "$fbBinDir"/../shared_functions.sh
+else
+# shellcheck source=shared_functions.sh
+  source "$fbBinDir"/../shared_functions.sh
+fi
 
 consoleHasInternet OneShot
 consoleFBfolderIsMounted OneShot
 
-# shellcheck disable=SC2034  # warning not relevant, var read by getopt(3)
-GETOPT_COMPATIBLE=true
-
 if [[ $# -eq 0 ]] ; then
-  echo -e "$PNAME : Too few arguments. At least I need a folder target \
+  echo -e >&2 "$PNAME : Too few arguments. At least I need a folder target \
 to backup.\nExecute \"$PNAME -h\" for help. Terminating..." >&2
  exit 2
 fi
-DEBUG=1
+
 help() {
 cat  << EOF
 
@@ -56,7 +91,7 @@ $PNAME:  Makes  a folder backup, part of the FB system.
 
 syntax:
 
-  $PNAME [options] <source folder>  <full-symlink-name>  <destination>
+  $PNAME [options] <source folder> <destination> <full-symlink-name>
   It is meant to be executed by *fboneshot* and not individually.
 
   Options:
@@ -68,6 +103,8 @@ syntax:
 
 EOF
 }
+# shellcheck disable=SC2034  # warning not relevant, var read by getopt(3)
+GETOPT_COMPATIBLE=true
 # set -x
 # https://stackoverflow.com/questions/402377/using-getopts-to-process-long-and-short-command-line-options
 TEMP=$(getopt -o hnvV --longoptions help,verbose,dry-run,version \
@@ -78,9 +115,6 @@ if [[ $? != 0 ]] ; then echo "Terminating..." >&2 ; exit 2 ; fi
 # Note the quotes around '$TEMP': they are essential!
 eval set -- "$TEMP"
 
-PARSE_DEBUG=1
-DRYRUN=false
-VERBOSE=false
 
 while true; do
     case "$1" in
@@ -93,12 +127,10 @@ while true; do
 done
 
 HAVING_ERRORS=false
-DEBUG=1
-
 
 if [[ $# -ne 3 ]] ; then
-  echo -e "$PNAME : Wrong number of  few arguments. I need one argument for \
-the folder to backup, the full symlink name, \nand the  path to the \
+  echo -e >&2 "$PNAME : Wrong number of  arguments. I need:\nOne argument for \
+the folder to backup,\nThe full symlink name, and:\nThe  path to the \
 destination folder of the backup operation. \
 \nExecute \"$PNAME -h\" for help. Terminating..." >&2
   exit 2
@@ -107,80 +139,47 @@ fi
 # we do check if the source folder exists. and that it doesn't exist
 # within the FB-backup tree.
 
-if [[ -d "$1" ]] ; then
-  TARGET_TEST="${1/$FB/}"
-  if [[ "$TARGET_TEST" != "$1" ]] ; then
-    if [[  "$DRYRUN" = true ]] ;  then
-      echo -e "$PNAME : The target folder IS inside $FB.\n(${1})."
-      HAVING_ERRORS=true
-    else
-      # same whether dry-run, verbose, or not.
-      echo -e "$PNAME : The target of the backup is not allowed to be inside \
-$FB.\nTerminating..."
-      exit 2
-    fi
-  else
-    if [[ $VERBOSE = true || $DEBUG -eq 0  ]] ;  then
-      echo -e "$PNAME : The target folder is NOT inside $FB.\n($1)."
-    elif [[ $DRYRUN = true ]] ; then
-      echo -e "$PNAME : The target folder is NOT inside $FB.\n($1)."
-    # else we're passing through further down the road.
-    fi
-  fi
-else
-  if [[ $DRYRUN = false ]] ; then
-    echo -e "$PNAME : The destination folder $2 does not exist.\
-      \nTerminating..."
-    exit 2
-  fi
-fi
-TARGET_FOLDER="$1"
+
+dieIfNotDirectoryExist "${1}"
+SOURCE_FOLDER="$1"
+dieIfSourceIsWithinFBTree "$SOURCE_FOLDER" "$BACKUP_SCHEME"
+
 
 # we check if the destination path exists, and within the FB-backup tree.
-if [[ -d "$2" ]] ; then
-  DEST_TEST="${2/$FB/}"
-  if [[ "$DEST_TEST" = "$2" ]] ; then
-      echo -e "$PNAME : The destination folder is NOT inside\n$FB\n which it\
+dieIfNotDirectoryExist "${2}"
+if ! isWithinPath "${2}" "$FB"; then
+  echo -e  >&2 "$PNAME : The destination folder is NOT inside\n$FB\n which it\
         must be. Terminating..."
-      exit 2
-  else
-    if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ;  then
-      echo -e "$PNAME : The destination folder is inside $FB.\n($2)."
-    fi
-    # else .. silently moving on ...
-  fi
+  exit 2
 else
-  if [[ $DRYRUN = false ]] ; then
-    echo -e "$PNAME : The destination folder $2 does not exist.\
-      \nTerminating..."
-    exit 2
+  if [[  $DEBUG -eq 0 ]] ;  then
+    echo -e >&2 "$PNAME : The destination folder is inside $FB.\n($2)."
   fi
 fi
 
 TODAYS_BACKUP_FOLDER="$2"
 # we need to check if the full symlink name is within the destination path.
 
-SYMLINK_TEST="${TODAYS_BACKUP_FOLDER/$3/}"
-if [[ "$SYMLINK_TEST" = "$TODAYS_BACKUP_FOLDER " ]] ; then
+if ! isWithinPath "$TODAYS_BACKUP_FOLDER" "${3}" ; then
   # same whether dry-run, verbose, or not.
-  echo -e "$PNAME : The full symlink name is not the correct one.\n It is\
-      not the name of the root folder of the backup.\nTerminating..."
+  echo -e >&2 "$PNAME : The full symlink name is not the correct one.\nIt is\
+not the name of the root folder of the backup.\nTerminating..."
   exit 2
 else
-  if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ;  then
-    echo -e "$PNAME : The full symlink name: $3 is the same as the name of\
-      the root folder of the backup:\n$TODAYS_BACKUP_FOLDER"
+  if [[ $DEBUG -eq 0 ]] ;  then
+    echo -e >&2 "$PNAME : The full symlink name: $3 is the same as the name \
+of the root folder of the backup:\n$TODAYS_BACKUP_FOLDER"
   fi
   # else .. silently moving on ...
 fi
-SYMLINK_NAME="$3"
+SYMLINK_NAME="${3}"
 
 # time to look for any exclude files
 
 if hasExcludeFile OneShot "$SYMLINK_NAME" ; then
- if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ; then
-   echo "$PNAME : I have an exclude file : $EXCLUDE_FILE "
-   cat "$EXCLUDE_FILE"
+ if [[  $DEBUG -eq 0 ]] ; then
+   echo >&2 "$PNAME : I have an exclude file : $EXCLUDE_FILE "
+   cat  >&2 "$EXCLUDE_FILE"
  fi
  EXCLUDE_OPTIONS="--exclude-from=$EXCLUDE_FILE"
 else
@@ -199,8 +198,8 @@ if [[ $DRYRUN = true  ]] ; then
   trap "HAVING_ERRORS=true;ctrl_c" INT
 
 ctrl_c() {
-  echo "$PNAME : trapped ctrl-c - interrupted tar command!"
-  echo "$PNAME : We: rm -fr $DRY_RUN_FOLDER."
+  echo >&2 "$PNAME : trapped ctrl-c - interrupted tar command!"
+  echo >&2  "$PNAME : We: rm -fr $DRY_RUN_FOLDER."
   rm -fr "$DRY_RUN_FOLDER"
 }
 
@@ -208,26 +207,30 @@ ctrl_c() {
 "$DRY_RUN_FOLDER"/"$(baseNameTimeStamped "$SYMLINK_NAME" )"-backup.tar.gz
   if [[ $HAVING_ERRORS = false ]] ; then
 
-    echo "$PNAME : sudo tar -z $VERBOSE_OPTIONS -c -f\
-      $TAR_BALL_NAME $EXCLUDE_OPTIONS -C $TARGET_FOLDER . "
-  #  | journalThis 7 OneShot
-    sudo tar -z  -c $VERBOSE_OPTIONS -f "$TAR_BALL_NAME" "$EXCLUDE_OPTIONS" \
--C "$TARGET_FOLDER" .
-   #   | journalThis 7 OneShot
+    echo >&2 "$PNAME : sudo tar -z $VERBOSE_OPTIONS -c -f \
+$TAR_BALL_NAME $EXCLUDE_OPTIONS -C $SOURCE_FOLDER . "
+
+    if [[ -n "$EXCLUDE_OPTIONS" ]] ; then
+      sudo tar -z  -c $VERBOSE_OPTIONS -f "$TAR_BALL_NAME" "$EXCLUDE_OPTIONS" \
+-C "$SOURCE_FOLDER" .
+    else
+      sudo tar -z  -c $VERBOSE_OPTIONS -f "$TAR_BALL_NAME" -C "$SOURCE_FOLDER" .
+    fi
+
     if [[ -d "$DRY_RUN_FOLDER" ]] ; then
         rm -fr "$DRY_RUN_FOLDER"
     fi
     EXIT_STATUS=$?
     if [[ $EXIT_STATUS -gt 1 ]] ; then
-      echo "$PNAME : exit status after tar commmand = $EXIT_STATUS"
-      echo "$PNAME : rm -fr $DRY_RUN_FOLDER"
+      echo >&2 "$PNAME : exit status after tar commmand = $EXIT_STATUS"
+      echo ">&2 $PNAME : rm -fr $DRY_RUN_FOLDER"
     fi
   else
-    echo -e "$PNAME \
+    echo -e >&2 "$PNAME \
 : DRY_RUN_FOLDER=\$(mktemp -d \"/tmp/OneShot.restore.sh.XXX\")"
-    echo -e "$PNAME : sudo tar -z -c $VERBOSE_OPTIONS -c  $EXCLUDE_OPTIONS -f\
-      $TAR_BALL_NAME  -C $TARGET_FOLDER . "
-    echo -e "$PNAME : rm -fr $DRY_RUN_FOLDER"
+    echo -e "$PNAME : sudo tar -z -c $VERBOSE_OPTIONS -c  $EXCLUDE_OPTIONS -f \
+$TAR_BALL_NAME  -C $SOURCE_FOLDER . "
+    echo -e >&2 "$PNAME : rm -fr $DRY_RUN_FOLDER"
   fi
 else
 # DRYRUN == false
@@ -236,33 +239,37 @@ else
 
   trap "HAVING_ERRORS=true;ctrl_c" INT
 ctrl_c() {
-  echo trapped ctrl-c
-  echo rm -f "$TAR_BALL_NAME"
+  echo >&2 trapped ctrl-c
+  echo >&2 rm -f "$TAR_BALL_NAME"
   rm -f "$TAR_BALL_NAME"
 }
     if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ; then
-      echo -e "$PNAME : sudo tar -z -c $VERBOSE_OPTIONS -c  $EXCLUDE_OPTIONS\
-        -f $TAR_BALL_NAME  -C $TARGET_FOLDER" .
+      echo -e >&2 "$PNAME : sudo tar -z -c $VERBOSE_OPTIONS -c  $EXCLUDE_OPTIONS\
+        -f $TAR_BALL_NAME  -C $SOURCE_FOLDER" .
     fi
-    sudo tar -z $VERBOSE_OPTIONS -c "$EXCLUDE_OPTIONS" -f "$TAR_BALL_NAME" -C\
-"$TARGET_FOLDER" .
+    if [[ -n "$EXCLUDE_OPTIONS" ]] ; then 
+      sudo tar -z $VERBOSE_OPTIONS -c "$EXCLUDE_OPTIONS" -f "$TAR_BALL_NAME" -C\
+"$SOURCE_FOLDER" .
+    else
+      sudo tar -z $VERBOSE_OPTIONS -c -f "$TAR_BALL_NAME" -C "$SOURCE_FOLDER" .
+    fi
     EXIT_STATUS=$?
-   #   | journalThis 7 OneShot
+
     if [[ $EXIT_STATUS -gt 1 ]] ; then
 
       if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ; then
-        echo "$PNAME : exit status after tar commmand (fatal error)\
+        echo >&2 "$PNAME : exit status after tar commmand (fatal error)\
           = $EXIT_STATUS"
       fi
 
       if [[ -f "$TAR_BALL_NAME" ]] ; then
         if [[ $VERBOSE = true || $DEBUG -eq 0 ]] ; then
-          echo "$PNAME : rm -f $TAR_BALL_NAME"
+          echo >&2 "$PNAME : rm -f $TAR_BALL_NAME"
         fi
         rm -f "$TAR_BALL_NAME"
       fi
     elif [[ $EXIT_STATUS -eq 0 ]] ; then
-      echo -e "\n($TAR_BALL_NAME)\n"
+      echo -e >&2 "\n($TAR_BALL_NAME)\n"
     fi
 fi
 
