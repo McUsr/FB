622fedc522c1581ae8ec2d3176ad59d08c2078c7
McUsr <tommy.bollman@gmail.com>
2023-03-10 17:57:07 +0100

    I have tested symlinkInJobsFolder and it works.

--- .local/bin/fb/loc_param.sh
+++ .local/bin/fb/loc_param.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
-source shared_functions.sh
-# motivasjonen er at uansett hvordan vi spesifiserer lokasjon, så skal vi klare
-# å oversette denne til det som ligger i jobsfolder for backup-scheme.
-# Vi returnerer symlink, hvis vi har. ellers så returnerer vi ingen ting.
-# Vi finner ikke .pause fil som er basert på  symlink her.
+source ~/.local/bin/fb/shared_functions.sh
 
-
-
-# Tanken bak det første forsoeket er å finne ut om er inne i.
-
-# Andre forsøk, om er symlink eller ikke.
-
-# symlink | inne i jobsfolder
-# ---------------------------
-#    x    |      x
-# ---------------------------
-#    x    |
-# ---------------------------
-#         |      x
-# ---------------------------
-#         |
-# ---------------------------
-
+# symlinkInJobsFolder()
+# We figure out if whatever is passed as the location represents a symlink
+# in the current jobs folder.
+# PARAMETERS: backupscheme location parameter:
+# ALGORITHM:
 # we check if the given parameter exists
 # 
 #   if it doesn't:
@@ -37,66 +21,71 @@ source shared_functions.sh
 #     it was a symlink:
 #       does it exist within jobsfolder?
 #         return it, or give error message
-
-backup_scheme="${1}"
-
-loc_param="${2}"
-
-echo BackupScheme : "$backup_scheme"
-
-jobs_folder="$XDG_DATA_HOME"/fbjobs/"$backup_scheme"
-
-echo JobsFolder : "$jobs_folder"
-
+#
+# RETURNS: A valid symlink, with full path, if any.
 
 # Does the file exist? if not, we might use the  jobs folder as a second chance
+symlinkInJobsFolder() {
 
-if [[ ! -r "$loc_param" ]] ; then
-  echo "$loc_param" : not found.
- candidate="$jobs_folder"/"$loc_param"
- echo "and the candidate is: $candidate"
-
-  if [[ ! -r "$candidate" ]] ; then
-      echo -e >&2 "$PNAME : $candidate doesn't exist, unresolvable conflict.\n\
-Please specify another parameter.\nTerminating"
-      exit 2
-  else
-    echo "found $candidate"
-    if ! isASymlink "$candidate" ; then
-      echo -e >&2 "$PNAME : $candidate isn't a symlink, unresolvable conflict.\n\
-Please specify another parameter.\nTerminating"
-      exit 2
-    else
-      echo "$candidate"
-    fi
+  if [[ $# -ne 2   ]] ; then
+    echo -e >&2 "I need two parameters!\nTerminating..."
+    exit 2
   fi
-else
-  echo >&2 " the parameter existed"
 
-  #  we get the real path
-  full_path="$(realpath "$loc_param")"
-  # we create a symlink of it.
-  if ! isASymlink "$full_path" ; then
-    # we need to create its symlink
-    symlinkName="$(fullPathSymlinkName "$full_path")"
-    echo >&2 "Da symlinkname: $symlinkName"
-    candidate="$jobs_folder"/"$symlinkName"
+  backup_scheme="${1}"
+  loc_param="${2}"
+  jobs_folder="$XDG_DATA_HOME"/fbjobs/"$backup_scheme"
+
+  echo BackupScheme : "$backup_scheme"
+  echo JobsFolder : "$jobs_folder"
+
+  if [[ ! -r "$loc_param" ]] ; then
+    echo "$loc_param" : not found.
+   candidate="$jobs_folder"/"$loc_param"
+   echo "and the candidate is: $candidate"
+
     if [[ ! -r "$candidate" ]] ; then
-      echo >&2 "$loc_param  isn't a backup job"
-      exit 2
+        echo -e >&2 "$PNAME : $candidate doesn't exist, unresolvable conflict.\n\
+  Please specify another parameter.\nTerminating"
+        exit 2
     else
-      echo "$candidate"
+      echo "found $candidate"
+      if ! isASymlink "$candidate" ; then
+        echo -e >&2 "$PNAME : $candidate isn't a symlink, unresolvable conflict.\n\
+  Please specify another parameter.\nTerminating"
+        exit 2
+      else
+        echo "$candidate"
+      fi
     fi
   else
-    candidate="$full_path"
-    probe=${candidate/$jobs_folder/}
-#    probe=${jobs_folder/$loc_param/}
-    echo >&2 Probe : $probe
-    if [[ "$probe" !=  "$candidate" ]] ; then
-      # its within, and a valid symlink
-      echo $candidate
+    echo >&2 " the parameter existed"
+
+    #  we get the real path
+    full_path="$(realpath "$loc_param")"
+    # we create a symlink of it.
+    if ! isASymlink "$full_path" ; then
+      # we need to create its symlink
+      symlinkName="$(fullPathSymlinkName "$full_path")"
+      echo >&2 "Da symlinkname: $symlinkName"
+      candidate="$jobs_folder"/"$symlinkName"
+      if [[ ! -r "$candidate" ]] ; then
+        echo >&2 "$loc_param  isn't a backup job"
+        exit 2
+      else
+        echo "$candidate"
+      fi
     else
-      echo >&2 "the symlink given is not the correct one for the path given"
+      candidate="$full_path"
+      probe=${candidate/$jobs_folder/}
+      echo >&2 Probe : $probe
+      if [[ "$probe" !=  "$candidate" ]] ; then
+        # its within, and a valid symlink
+        echo $candidate
+      else
+        echo >&2 "the symlink given is not the correct one for the path given"
+      fi
     fi
   fi
-fi
+}
+symlinkInJobsFolder $*
