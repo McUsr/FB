1acd2dd074428d418918acf1bb675987f128f6fd
McUsr <tommy.bollman@gmail.com>
2023-03-13 19:43:34 +0100

    Removed merge conflicts.

--- .local/bin/fb/loc_param.sh
+++ .local/bin/fb/loc_param.sh
@@ -1,14 +1,33 @@
-#!/bin/bash
+#         !/bin/bash
 
 source ~/.local/bin/fb/shared_functions.sh
 
+err() {
+  if [[ $# -ne 2 ]] ; then 
+    echo -e >&2 "$PNAME/${FUNCNAME[0]} : I need two parameters!\nTerminating"
+  else
+    echo -e >&2 "$PNAME/${FUNCNAME[$1]} : $2\nTerminating"
+  fi
+}
+
+nrargs() {
+  if [[ $# -ne 3 ]] ; then
+    err 1 "I really need 3 arguments!"
+    return 1
+  elif [[ $1 -ne $2 ]] ; then 
+    err 2  "$3"
+    return 1
+  fi
+  return 0
+}
 # symlinkInJobsFolder()
 # We figure out if whatever is passed as the location represents a symlink
 # in the current jobs folder.
-# PARAMETERS: backupscheme location parameter:
+# PARAMETERS: backupscheme, location_parameter
+#
 # ALGORITHM:
 # we check if the given parameter exists
-# 
+#
 #   if it doesn't:
 #     does it exist within the job folder?
 #       return it, or give error message
@@ -27,14 +46,14 @@ source ~/.local/bin/fb/shared_functions.sh
 # Does the file exist? if not, we might use the  jobs folder as a second chance
 symlinkInJobsFolder() {
 
-  if [[ $# -ne 2   ]] ; then
-    echo -e >&2 "I need two parameters!\nTerminating..."
+LDBG=1
+  if ! nrargs 3 $#  "I need three arguments!" ; then
     exit 2
   fi
 
   backup_scheme="${1}"
   loc_param="${2}"
-  jobs_folder="$XDG_DATA_HOME"/fbjobs/"$backup_scheme"
+  jobs_folder="${3}"
 
   echo >&2 BackupScheme : "$backup_scheme"
   echo >&2 JobsFolder : "$jobs_folder"
@@ -45,21 +64,21 @@ symlinkInJobsFolder() {
    echo >&2 "and the candidate is: $candidate"
 
     if [[ ! -r "$candidate" ]] ; then
-        echo -e >&2 "$PNAME : $candidate doesn't exist, unresolvable conflict.\n\
-  Please specify another parameter.\nTerminating"
+        err 1 "$candidate doesn't exist, unresolvable conflict.\n\
+  Please specify another parameter."
         exit 2
     else
       echo >&2 "found $candidate"
       if ! isASymlink "$candidate" ; then
-        echo -e >&2 "$PNAME : $candidate isn't a symlink, unresolvable conflict.\n\
-  Please specify another parameter.\nTerminating"
+        err 1 "$candidate isn't a symlink, unresolvable conflict.\n\
+  Please specify another parameter."
         exit 2
       else
         echo "$candidate"
       fi
     fi
   else
-    echo >&2 " the parameter existed"
+    if [[ $LDBG -eq 0 ]] ; then echo >&2 " the parameter existed" ; fi
 
     #  we get the real path
     full_path="$(realpath "$loc_param")"
@@ -67,10 +86,10 @@ symlinkInJobsFolder() {
     if ! isASymlink "$full_path" ; then
       # we need to create its symlink
       symlinkName="$(fullPathSymlinkName "$full_path")"
-      echo >&2 "Da symlinkname: $symlinkName"
+      if [[ $LDBG -eq 0 ]] ; then echo >&2 "Da symlinkname: $symlinkName" ; fi
       candidate="$jobs_folder"/"$symlinkName"
       if [[ ! -r "$candidate" ]] ; then
-        echo >&2 "$loc_param  isn't a backup job"
+        err 1 "$loc_param  isn't a backup job!"
         exit 2
       else
         echo "$candidate"
@@ -78,14 +97,14 @@ symlinkInJobsFolder() {
     else
       candidate="$full_path"
       probe=${candidate/$jobs_folder/}
-      echo >&2 Probe : $probe
+      if [[ $LDBG -eq 0 ]] ; then echo >&2 Probe : $probe ; fi
       if [[ "$probe" !=  "$candidate" ]] ; then
         # its within, and a valid symlink
         echo $candidate
       else
-        echo >&2 "the symlink given is not the correct one for the path given"
+        err 1 "the symlink given is not the correct one for the path given"
+        exit 2
       fi
     fi
   fi
 }
-# symlinkInJobsFolder $*
